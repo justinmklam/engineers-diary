43a6c21be20908531bfc378057b27fadf9941a9a
justinmklam <justinmklam@gmail.com>
2016-09-22 22:51:59 -0700

    Revised layout and minor fixes
    Changed icon and added Log Archive back to tabstop.

--- Engineers-Diary/Form1.Designer.cs
+++ Engineers-Diary/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtAuthor = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -39,11 +40,12 @@
             this.txtEntry = new System.Windows.Forms.TextBox();
             this.btnCommit = new System.Windows.Forms.Button();
             this.lstArchive = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtAuthor
             // 
-            this.txtAuthor.Location = new System.Drawing.Point(292, 327);
+            this.txtAuthor.Location = new System.Drawing.Point(300, 327);
             this.txtAuthor.Name = "txtAuthor";
             this.txtAuthor.Size = new System.Drawing.Size(93, 20);
             this.txtAuthor.TabIndex = 2;
@@ -51,7 +53,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(247, 328);
+            this.label5.Location = new System.Drawing.Point(297, 310);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 13);
             this.label5.TabIndex = 10;
@@ -60,7 +62,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(201, 11);
+            this.label4.Location = new System.Drawing.Point(99, 11);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 7;
@@ -69,7 +71,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 11);
+            this.label3.Location = new System.Drawing.Point(10, 11);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 6;
@@ -78,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 230);
+            this.label2.Location = new System.Drawing.Point(99, 210);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 8;
@@ -87,7 +89,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 328);
+            this.label1.Location = new System.Drawing.Point(99, 310);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 9;
@@ -97,20 +99,20 @@
             // 
             this.txtSubject.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.txtSubject.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
-            this.txtSubject.Location = new System.Drawing.Point(81, 326);
+            this.txtSubject.Location = new System.Drawing.Point(102, 327);
             this.txtSubject.Name = "txtSubject";
-            this.txtSubject.Size = new System.Drawing.Size(152, 20);
+            this.txtSubject.Size = new System.Drawing.Size(189, 20);
             this.txtSubject.TabIndex = 1;
             // 
             // txtArchiveEntry
             // 
             this.txtArchiveEntry.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtArchiveEntry.Location = new System.Drawing.Point(204, 27);
+            this.txtArchiveEntry.Location = new System.Drawing.Point(102, 30);
             this.txtArchiveEntry.Multiline = true;
             this.txtArchiveEntry.Name = "txtArchiveEntry";
             this.txtArchiveEntry.ReadOnly = true;
             this.txtArchiveEntry.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtArchiveEntry.Size = new System.Drawing.Size(267, 186);
+            this.txtArchiveEntry.Size = new System.Drawing.Size(374, 168);
             this.txtArchiveEntry.TabIndex = 5;
             this.txtArchiveEntry.TabStop = false;
             // 
@@ -118,10 +120,10 @@
             // 
             this.txtEntry.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.txtEntry.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
-            this.txtEntry.Location = new System.Drawing.Point(81, 230);
+            this.txtEntry.Location = new System.Drawing.Point(102, 226);
             this.txtEntry.Multiline = true;
             this.txtEntry.Name = "txtEntry";
-            this.txtEntry.Size = new System.Drawing.Size(390, 84);
+            this.txtEntry.Size = new System.Drawing.Size(377, 77);
             this.txtEntry.TabIndex = 0;
             this.txtEntry.TextChanged += new System.EventHandler(this.txtEntry_TextChanged);
             this.txtEntry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntry_KeyDown);
@@ -129,7 +131,7 @@
             // btnCommit
             // 
             this.btnCommit.Enabled = false;
-            this.btnCommit.Location = new System.Drawing.Point(396, 325);
+            this.btnCommit.Location = new System.Drawing.Point(401, 325);
             this.btnCommit.Name = "btnCommit";
             this.btnCommit.Size = new System.Drawing.Size(75, 23);
             this.btnCommit.TabIndex = 3;
@@ -140,18 +142,29 @@
             // lstArchive
             // 
             this.lstArchive.FormattingEnabled = true;
-            this.lstArchive.Location = new System.Drawing.Point(18, 27);
+            this.lstArchive.Location = new System.Drawing.Point(13, 30);
             this.lstArchive.Name = "lstArchive";
-            this.lstArchive.Size = new System.Drawing.Size(167, 186);
+            this.lstArchive.Size = new System.Drawing.Size(74, 316);
             this.lstArchive.TabIndex = 4;
-            this.lstArchive.TabStop = false;
             this.lstArchive.SelectedIndexChanged += new System.EventHandler(this.lstArchive_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(392, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 14);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Version 0.3";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 361);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtAuthor);
             this.Controls.Add(this.label4);
@@ -163,6 +176,7 @@
             this.Controls.Add(this.txtEntry);
             this.Controls.Add(this.btnCommit);
             this.Controls.Add(this.lstArchive);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Engineer\'s Diary";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
@@ -184,6 +198,7 @@
         private System.Windows.Forms.TextBox txtEntry;
         private System.Windows.Forms.Button btnCommit;
         private System.Windows.Forms.ListBox lstArchive;
+        private System.Windows.Forms.Label label6;
     }
 }
 
