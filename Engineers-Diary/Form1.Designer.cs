ce4d0ece5144c9bc29170242cbe1b135044cafe9
justinmklam <justinmklam@gmail.com>
2016-09-22 22:03:38 -0700

    First commit (migration from BitBucket)

---
+++ Engineers-Diary/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Engineers_Diary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtArchiveEntry = new System.Windows.Forms.TextBox();
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.lstArchive = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(292, 327);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(93, 20);
+            this.txtAuthor.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(247, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Author:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Log Entry:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Log Archive:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Description:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Project(s):";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtSubject.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.txtSubject.Location = new System.Drawing.Point(81, 326);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(152, 20);
+            this.txtSubject.TabIndex = 1;
+            // 
+            // txtArchiveEntry
+            // 
+            this.txtArchiveEntry.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArchiveEntry.Location = new System.Drawing.Point(204, 27);
+            this.txtArchiveEntry.Multiline = true;
+            this.txtArchiveEntry.Name = "txtArchiveEntry";
+            this.txtArchiveEntry.ReadOnly = true;
+            this.txtArchiveEntry.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtArchiveEntry.Size = new System.Drawing.Size(267, 186);
+            this.txtArchiveEntry.TabIndex = 5;
+            this.txtArchiveEntry.TabStop = false;
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtEntry.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.txtEntry.Location = new System.Drawing.Point(81, 230);
+            this.txtEntry.Multiline = true;
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(390, 84);
+            this.txtEntry.TabIndex = 0;
+            this.txtEntry.TextChanged += new System.EventHandler(this.txtEntry_TextChanged);
+            this.txtEntry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntry_KeyDown);
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Enabled = false;
+            this.btnCommit.Location = new System.Drawing.Point(396, 325);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(75, 23);
+            this.btnCommit.TabIndex = 3;
+            this.btnCommit.Text = "Commit";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // lstArchive
+            // 
+            this.lstArchive.FormattingEnabled = true;
+            this.lstArchive.Location = new System.Drawing.Point(18, 27);
+            this.lstArchive.Name = "lstArchive";
+            this.lstArchive.Size = new System.Drawing.Size(167, 186);
+            this.lstArchive.TabIndex = 4;
+            this.lstArchive.TabStop = false;
+            this.lstArchive.SelectedIndexChanged += new System.EventHandler(this.lstArchive_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 361);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtArchiveEntry);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.lstArchive);
+            this.Name = "Form1";
+            this.Text = "Engineer\'s Diary";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtArchiveEntry;
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.Button btnCommit;
+        private System.Windows.Forms.ListBox lstArchive;
+    }
+}
+
---
+++ Engineers-Diary/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Engineers_Diary
+{
+    public partial class Form1 : Form
+    {
+        string path_archive = @"Engineers-Diary-Archive";
+
+        public virtual bool ShortcutsEnabled { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            updateArchiveList();
+            txtAuthor.Text = Properties.Settings.Default.author;
+        }
+
+        private void txtEntry_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtEntry.Text))
+                btnCommit.Enabled = true;
+            else
+                btnCommit.Enabled = false;
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            List<string> to_write = new List<string>();
+
+            if (!Directory.Exists(path_archive))
+                Directory.CreateDirectory(path_archive);
+
+            string filename = DateTime.Now.ToString("yyyy-MM-dd");
+            string timestamp = DateTime.Now.ToString("h:mm:ss tt - ");
+
+            string filename_toread = path_archive + "\\" + filename + ".txt";
+
+            if (File.Exists(filename_toread))
+            {
+                to_write = File.ReadAllLines(filename_toread).ToList();
+            }
+            else
+            {
+                to_write.AddRange(constructHeader());
+                lstArchive.Items.Insert(0, filename);
+            }
+            
+            to_write.Add(timestamp + txtEntry.Text);
+            to_write.Add("");
+
+            filename += ".txt";
+
+            System.IO.File.WriteAllLines(path_archive + "\\" + filename, to_write);
+
+            txtEntry.Clear();
+            txtSubject.Clear();
+            txtArchiveEntry.Text = System.IO.File.ReadAllText(path_archive + "\\" + filename);
+        }
+
+        private List<string> constructHeader()
+        {
+            List<string> header = new List<string>();
+            header.Add("---");
+            header.Add("DATE: " + DateTime.Now.ToString("ddd, d MMM yyyy"));
+            //header.Add("TIME: " + DateTime.Now.ToString("h:mm:ss tt"));
+            header.Add("AUTHOR: " + txtAuthor.Text);
+            header.Add("---");
+            header.Add("");
+            header.Add("PROJECTS: " + txtSubject.Text);
+            header.Add("");
+
+            return header;
+        }
+
+        private void updateArchiveList()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(path_archive);
+            try
+            {
+                FileInfo[] Files = dinfo.GetFiles("*.txt");
+
+                foreach (FileInfo file in Files)
+                {
+                    lstArchive.Items.Insert(0, Path.GetFileNameWithoutExtension(file.Name));
+                }
+            }
+            catch (DirectoryNotFoundException) { }
+
+        }
+
+        private void lstArchive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtArchiveEntry.Clear();
+
+            try
+            {
+                string file_to_read = lstArchive.SelectedItem.ToString() + ".txt";
+                //string[] lines = System.IO.File.ReadAllLines(path_archive + "\\" + file_to_read);
+
+                txtArchiveEntry.Text = System.IO.File.ReadAllText(path_archive + "\\" + file_to_read);
+            }
+            catch (NullReferenceException) { }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.author = txtAuthor.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void txtEntry_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control & e.KeyCode == Keys.A)
+            {
+                ((TextBox)sender).SelectAll();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control & e.KeyCode == Keys.Back)
+            {
+                SendKeys.SendWait("^+{LEFT}{BACKSPACE}{BACKSPACE}");
+            }
+        }
+    }
+}
